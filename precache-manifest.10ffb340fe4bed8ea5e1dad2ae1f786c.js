32dafa298e6bd2b6f180410ae0611067eebfc74d
wongjx <wongjx92@gmail.com>
2019-01-18 11:43:47 +0800

    Updates

---
+++ precache-manifest.10ffb340fe4bed8ea5e1dad2ae1f786c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a9df9a4c541d744a7ef",
+    "url": "/react_practice_todolist/static/js/runtime~main.8a9df9a4.js"
+  },
+  {
+    "revision": "82e91be50a4992d5dd52",
+    "url": "/react_practice_todolist/static/js/main.82e91be5.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/react_practice_todolist/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "82e91be50a4992d5dd52",
+    "url": "/react_practice_todolist/static/css/main.b2558e0d.chunk.css"
+  },
+  {
+    "revision": "52dec1159736d4bf9d77c129fd04eaf2",
+    "url": "/react_practice_todolist/index.html"
+  }
+];
